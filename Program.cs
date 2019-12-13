c9c74c1384d01d5ae44879da9da9233e85c9939a
hladan <hamed.ladan@gmail.com>
2019-12-13 13:32:48 +0330

    First commit!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sample_code_desktop_repository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
